dace6554d15064691a395816979474354bbc1597
Al-dento <118441501+Al-dento@users.noreply.github.com>
2023-01-31 14:10:48 +0300

    Update FirstApplication.java

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -2,6 +2,6 @@ package com.mjc.stage0;
 
 public class FirstApplication {
     public static void main(String[] args) {
-        System.our.println("Hello, world!");
+        System.out.println("Hello, world!");
     }
 }
